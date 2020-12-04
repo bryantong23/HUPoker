390c366eef338a29bc68fe19881dda976484a8ca
Bryan Tong <bryantong1257@gmail.com>
2020-12-04 14:07:01 -0500

    Add game settings section

--- src/App.js
+++ src/App.js
@@ -8,9 +8,17 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      smallBlind: 5,
+      bigBlind: 10,
+      startingStack: 1000,
       players: [
-        { id: 1, name: "Type your name here", stackSize: 1000, viewText: false },
-        { id: 2, name: "Bot", stackSize: 0 },
+        {
+          id: 1,
+          name: "Type your name here",
+          stackSize: 1000,
+          viewText: false,
+        },
+        { id: 2, name: "Bot", stackSize: 1000 },
       ],
     };
   }
@@ -20,25 +28,59 @@ class App extends Component {
   };
 
   handleClickRaise = () => {
-      const players = this.state.players;
-      players[0].viewText = !players[0].viewText;
-      this.setState({players});
+    const players = this.state.players;
+    players[0].viewText = !players[0].viewText;
+    this.setState({ players });
   };
 
   handleRaise = (amount) => {
     const players = this.state.players;
     players[0].stackSize -= parseInt(amount);
-    this.setState({players});
-  }
+    players[0].viewText = !players[0].viewText;
+    this.setState({ players });
+  };
 
   handleFold = () => {
     console.log("Fold");
   };
 
+  update = () => {
+    console.log("Update")
+    const sb = parseInt(document.getElementById("sb").value);
+    const bb = parseInt(document.getElementById("bb").value);
+    const ss = parseInt(document.getElementById("ss").value);
+    this.setState({smallBlind: sb, bigBlind: bb, startingStack: ss});
+    const players = this.state.players;
+    for (var i = 0; i < this.state.players.length; i++){
+      players[i].stackSize = ss;
+    }
+    this.setState({players});
+  };
+
   render() {
     return (
       <React.Fragment>
-        <header><h1>HUPoker</h1></header>
+        <header>
+          <h1>HUPoker</h1>
+          <div>
+            <b>Game settings:</b><br></br>
+            <label htmlFor="sb">Small blind:</label>
+            <input type="text" id="sb" name="sb" defaultValue="5"></input>
+            <br></br>
+            <label htmlFor="bb">Big blind:</label>
+            <input type="text" id="bb" name="bb" defaultValue="10"></input>
+            <br></br>
+            <label htmlFor="ss">Stack size:</label>
+            <input type="text" id="ss" name="ss" defaultValue="1000"></input>
+            <br></br>
+            <button
+              className="btn btn-primary btn-sm m-2"
+              onClick={this.update}
+            >
+              Update
+            </button>
+          </div>
+        </header>
         <main className="container">
           <Players
             players={this.state.players}
